ebe1af8b939708d1f81978a9a6234eab151229c8
icunha-t <isabeltootill@gmail.com>
2026-04-19 15:55:47 +0100

    addition of packages for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
+  return(
     <Router>
-      {/* This is a simple Navigation bar that stays visible on all pages */}
-      <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+      <nav style={{padding: "1rem", borderBottom: "1px solid #ccc" }}>
         <Link style={{ marginRight: "1rem" }} to="/home">Home</Link>
       </nav>
 
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </Router>
   );
