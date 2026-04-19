c37aa44ac1471129eb341b27cd608407e597f27d
icunha-t <isabeltootill@gmail.com>
2026-04-19 15:42:37 +0100

    added route possibility; ready for home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>This will be a homepage soon!</h1>
-    </>
-  )
+    <Router>
+      {/* This is a simple Navigation bar that stays visible on all pages */}
+      <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+        <Link style={{ marginRight: "1rem" }} to="/home">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
