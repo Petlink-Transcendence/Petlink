a6a282cb3e5b4c0e2d7f1544614790fe4a15de37
isabeltootill <isabel.tootill@bmwgroup.com>
2026-04-24 00:45:51 +0100

    login page v1

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 export default function Header() {
   return (
-    <>
-      <h1>This will be a Header soon!</h1>
-    </>
-  )
+    <header className="main-header">
+      {/* Left Side: Logo */}
+      <Link to="/" className="header-logo">
+        <img src="../public/favicon1.png" alt="PetLink" />
+      </Link>
+      
+      {/* Right Side: Navigation Links */}
+      <nav className="header-nav">
+        <Link to="/login" className="nav-item">Login</Link>
+        <Link to="/notifications" className="nav-item">Notifications</Link>
+        <Link to="/" className="nav-item home-link">Home</Link>
+      </nav>
+    </header>
+  );
 }
-
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,9 +8,33 @@ export default function Login() {
   
     return (
       <>
-        <div className="login-wrapper">
-        <h1>Login Page</h1>
-        <button className="login-button"> Sign in with 42</button>
+        <div className="login-page-container">
+
+          {/*Welcome message*/}
+          <h1 className="welcome-message">Welcome to <span className="welcome-message-petlink">PetLink</span>
+          </h1>
+
+          {/*Centered Login box */}
+          <div className="login-box">
+
+            <h2 className="login-title">Login</h2>
+
+            <h2 className="sign-in-message">Sign in to continue</h2>
+
+            <div className="input-group">
+              <label>Email</label>
+              <input type="email" placeholder="youremail@email.com" />
+            </div>
+
+            <div className="input-group">
+              <label>Password</label>
+              <input type="password" placeholder="***************" />
+            </div>
+
+
+            <h2 className="sign-up-message"> Don't have a <span className="sign-up-message-petlink">PetLink</span> account yet? </h2>
+            <button className="login-button"> Sign up here </button>
+          </div>
         </div>
       </>
     )
