f4d26cf129fec41b3d89859dda4e8306437fee57
Daniela-Padilha <danielasofiapadilha@gmail.com>
2026-04-25 19:39:59 +0100

    removed hardcoded values in post component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -16,9 +16,43 @@ export default function Home() {
       <div className="home-post-container">
         <CreatePost />
 
-        <Post />
-        <Post />
-        <Post />
+        <Post
+        name="Jane Doe"
+        tag="NEED SITTER"
+        text="Looking for a sitter for my cat Luna next week!"
+        location="Porto, PT"
+        time="2h ago"
+        tags={[
+         "May 1-5",
+          "15€-20€/day",
+          "Luna | Bengal Cat | 2yr",
+          "Cat experience required",
+          ]}
+        />
+        <Post
+        name="John Smith"
+        tag="NEED WALKER"
+        text="Dog needs walking every morning."
+        location="Lisbon, PT"
+        time="5h ago"
+        tags={[
+         "May 1-5",
+          "15€-20€/day",
+          "Energy required",
+          "Dog experience required",
+          ]}
+        />
+        <Post
+        name="Junior Silva"
+        tag="SITTER"
+        text="I'm a sitter with a lot of experience with dogs!"
+        location="Rio de Janeiro, BR"
+        time="11min ago"
+        tags={[
+          "20€-25€/day",
+          "Dog experience",
+          ]}
+        />
       </div>
     </div>
   )
