9b870e3f808da305f8b43aebfd328fb2dae9dad7
Daniela-Padilha <danielasofiapadilha@gmail.com>
2026-04-25 18:43:10 +0100

    added post section and separated components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import './Home.css'
+import ProfileCard from '../components/homepage/ProfileCard'
+import Post from '../components/homepage/Post'
 import { useEffect } from 'react'
 
 export default function Home() {
@@ -8,31 +10,9 @@ export default function Home() {
     }, []);
 
   return (
-    <>
-      <div className="profile-card-container">
-        <div className="profile-card">
-          <img className="profile-pic" alt="profile picture" src="https://4kwallpapers.com/images/wallpapers/cat-kitten-pet-domestic-animals-cute-cat-portrait-fur-baby-1280x1280-3528.jpg"></img>
-          <h4 className="name">Jane Doe</h4>
-          <p className="username">@janedoe123</p>
-          <p className="bio">Dog and cat mom. Always looking for the best care for my fur babies</p>
-          <hr className="divider" />
-          <div className="stats">
-            <div className="stats-nbrs">
-              <p className="nbr">21</p>
-              <p>Posts</p>
-            </div>
-            <div className="stats-nbrs">
-              <p className="nbr">42</p>
-              <p>Followers</p>
-            </div>
-            <div className="stats-nbrs">
-              <p className="nbr">100</p>
-              <p>Following</p>
-            </div>
-  
-        </div>
-        </div>
-      </div>
-    </>
+    <div className="home-container">
+      <ProfileCard />
+      <Post />
+    </div>
   )
 }
