46c950e60b94aca87c558ebd40435b3c5fc212c1
Daniela-Padilha <danielasofiapadilha@gmail.com>
2026-04-25 17:46:55 +0100

    added profile card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,7 +8,30 @@ export default function Home() {
   
   return (
     <>
-      <h1>This will be a homepage soon!</h1>
+      <div className="profile-card-container">
+        <div className="profile-card">
+          <img className="profile-pic" alt="profile picture" src="https://4kwallpapers.com/images/wallpapers/cat-kitten-pet-domestic-animals-cute-cat-portrait-fur-baby-1280x1280-3528.jpg"></img>
+          <h4 className="name">Jane Doe</h4>
+          <p className="username">@janedoe123</p>
+          <p className="bio">Dog and cat mom. Always looking for the best care for my fur babies</p>
+          <hr className="divider" />
+          <div className="stats">
+            <div className="stats-nbrs">
+              <p className="nbr">21</p>
+              <p>Posts</p>
+            </div>
+            <div className="stats-nbrs">
+              <p className="nbr">42</p>
+              <p>Followers</p>
+            </div>
+            <div className="stats-nbrs">
+              <p className="nbr">100</p>
+              <p>Following</p>
+            </div>
+  
+        </div>
+        </div>
+      </div>
     </>
   )
 }
