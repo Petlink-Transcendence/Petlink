c05c4176d38e1344df5d76328def32e440396ca2
Isabel Tootill <isabeltootill@gmail.com>
2026-05-01 23:44:51 +0100

    added react modules

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,11 @@ import './Home.css'
 import { useEffect } from 'react'
 
 export default function Home() {
+
+  useEffect(() => {
+        document.title = "Login | PetLink";
+    }, []);
+
   return (
     <div className="page-content"> {/* Adicione aqui */}
       <h1>This will be a homepage soon!</h1>
