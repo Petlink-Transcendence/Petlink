d082d69726fd432525f582de892c1b75f361d28e
Isabel Tootill <isabeltootill@gmail.com>
2026-05-10 19:01:01 +0100

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
+import Footer from "./components/Footer";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -37,6 +38,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+	<footer className="footer">
+      <div className="footer-content">
+        <p> 
+          <span className="footer-team"> Built with ❤️ by <span className="footer-brand">PetLink</span> </span>
+          <span className="footer-slogan"> Connecting pets, sitters & owners </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
