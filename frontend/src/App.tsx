612c005d58706834ca3003ef52b4b9197d229533
Daniela Padilha <danielasofiapadilha@gmail.com>
2026-05-10 14:06:36 +0100

    removed footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -36,7 +35,6 @@ function App() {
           } />
         </Routes>
       </div>
-      <Footer />
     </Router>
   );
 }
