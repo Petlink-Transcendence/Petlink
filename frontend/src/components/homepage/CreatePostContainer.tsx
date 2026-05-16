5a7559b58c8d7845baca04293e1f644cf9ce2f21
Isabel Tootill <isabeltootill@gmail.com>
2026-05-16 16:04:33 +0100

    added post container - css missing

--- frontend/src/components/homepage/CreatePostContainer.tsx
+++ frontend/src/components/homepage/CreatePostContainer.tsx
@@ -1,14 +1,21 @@
+import React, {use, useState} from 'react'
+import CreatePostContainer from './Post-popup'
 import './CreatePost.css'
 
-/* it will send text to Post eventually */
 export default function CreatePost() {
+	const [isPostOpen, setIsPostOpen] = useState(false);
+
 	return (
-	<div className="create-post">
-		<div
-			contentEditable
-			className="create-post-input"
-			data-placeholder="What's on your pet's mind?"
-		/>
-	</div>
+		<div className='create-post-container'>
+			<div className="create-post" onClick={() => setIsPostOpen(true)}>
+			<div
+				className="create-post-input"
+				data-placeholder="What's on your pet's mind?"
+			/>
+		</div>
+		{isPostOpen && (
+			<CreatePostContainer onClose={() => setIsPostOpen(false)} />
+		)}
+		</div>
 	);
 }
