990f0fea553cc3b03c647ad5dce6a737514b1c96
larocqueg <portugalgabriel2021@gmail.com>
2026-06-23 10:35:16 +0100

    changed default ports because some of them was not working in 42 pc

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     const handleLogin = async () => {
         setError('');
         try {
-            const response = await fetch('http://localhost/auth/login/', {
+            const response = await fetch('http://localhost:8080/auth/login/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
