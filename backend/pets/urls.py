616e42b728823e6d1397203481ba1ffaa9afcc53
jvieira96 <joaopfreirevieira@gmail.com>
2026-06-24 20:06:35 +0100

    feat(pets): add GET /api/users/{id}/pets/ list endpoint

--- backend/pets/urls.py
+++ backend/pets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PetCreateView
+from .views import PetCreateView, UserPetListView
 
 urlpatterns = [
-    path('pets/', PetCreateView.as_view(), name='pet-create')
+    path('pets/', PetCreateView.as_view(), name='pet-create'),
+    path('users/<int:pk>/pets/', UserPetListView.as_view(), name='users-pet-list')
 ]
--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Pet, UserPet
 from .serializers import PetSerializer
 
@@ -12,3 +12,13 @@ class PetCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         pet = serializer.save()
         UserPet.objects.create(user=self.request.user, pet=pet)
+
+class UserPetListView(generics.ListAPIView):
+    """View for user pets list"""
+    serializer_class = PetSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        user_pet_links = UserPet.objects.filter(user_id=self.kwargs['pk'])
+        pet_ids = user_pet_links.values_list('pet_id', flat=True)
+        return Pet.objects.filter(id__in=pet_ids)
