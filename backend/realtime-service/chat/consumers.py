36bfcd4b6a7ac4450daa707f86115bec312636f2
Ricardo Garcia <costagarcia.r95@gmail.com>
2026-06-26 23:52:58 +0100

    refactor(chat/consumers.py): #74 handle connect

--- backend/realtime-service/chat/consumers.py
+++ backend/realtime-service/chat/consumers.py
@@ -6,7 +6,16 @@ from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.room_group_name = f'chat_{self.user_id}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
         await self.accept()
+        await self.update_online_status(True)
 
     async def disconnect(self, close_code):
         pass
