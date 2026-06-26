b175e3f757790deb97c614d67f98d307d89762c1
Ricardo Garcia <costagarcia.r95@gmail.com>
2026-06-26 23:23:28 +0100

    update(realtime-service/core): #74 update settings.py
    Remove 'postgres' and '5432' from DATABASES

--- backend/realtime-service/chat/consumers.py
+++ backend/realtime-service/chat/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.utils import timezone
+from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
