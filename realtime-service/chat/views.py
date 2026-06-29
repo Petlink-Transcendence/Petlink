b97cdeb31a1e7277b6ef200ab6402838772e4290
ricvrdv <costagarcia.r95@gmail.com>
2026-06-29 20:52:02 +0100

    feat(chat): add views for GET

--- realtime-service/chat/views.py
+++ realtime-service/chat/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Message
+from django.db.models import Q
 
-# Create your views here.
+@api_view(['GET'])
+def message_history(request, user_id):
+    logged_in_user = request.query_params.get('sender_id')
+
+    if not logged_in_user:
+        return Response(
+            {'error': 'sender_id is required'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    messages = Message.objects.filter(
+        Q(sender_id=logged_in_user, recipient_id=user_id) |
+        Q(sender_id=user_id, recipient_id=logged_in_user)
+    ).order_by('created_at')[:50]
+
+    data = [
+        {
+            'id': m.id,
+            'sender_id': m.sender_id,
+            'recipient_id': m.recipient_id,
+            'content': m.content,
+            'read_at': m.read_at,
+            'created_at': m.created_at,
+        }
+        for m in messages
+    ]
+
+    return Response(data, status=status.HTTP_200_OK)
