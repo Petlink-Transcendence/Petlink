5101a6f9546502f746fc70ea0ab78d2f7b3500e4
larocqueg <portugalgabriel2021@gmail.com>
2026-07-04 16:04:34 +0100

    changed backend core-service to port 8080, nginx.conf was rediecting to 8000 because the link was incomplete

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -109,6 +109,7 @@ REST_FRAMEWORK = {
 # CORS configuration to allow React access
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 # SimpleJWT configuration
