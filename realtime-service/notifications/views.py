e63fa34442e42103d495f69be5b8525d38b69853
Ricardo Garcia <costagarcia.r95@gmail.com>
2026-07-05 17:16:50 +0100

    feat(notifications): add GET endpoint - list notifications for logged in user

--- realtime-service/notifications/views.py
+++ realtime-service/notifications/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Notification
 
-# Create your views here.
+@api_view(['GET'])
+def list_notifications(request, user_id):
+    notifications = Notification.objects.filter(
+        user_id=user_id
+    ).order_by('-created_at')
+
+    data = [
+        {
+            'id': n.id,
+            'type': n.type,
+            'content': n.content,
+            'reference_id': n.reference_id,
+            'reference_type': n.reference_type,
+            'read': n.read,
+            'created_at': n.created_at,
+        }
+        for n in notifications
+    ]
+    return Response(data, status=status.HTTP_200_OK)
+
+
