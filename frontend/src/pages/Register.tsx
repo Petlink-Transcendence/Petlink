08fb2c9ac5d28a049f894197a93a2d64c221f118
Daniela Padilha <danielasofiapadilha@gmail.com>
2026-07-26 15:49:19 +0100

    responsiveness improved in sign up

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -75,7 +75,7 @@ export default function Register() {
     };
 
     return (
-        <div className="login-page-container">
+        <div className="login-page-container signup-page-container">
             <h1 className="welcome-message">
                 Welcome to <span className="welcome-message-petlink">PetLink</span>
             </h1>
